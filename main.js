874f019aea7d29c04aed43cc943479dfca8d7191
atrphiqtl <165547381+atrphiqtl@users.noreply.github.com>
2025-11-11 21:06:43 -0800

    Add mouseover/mouseout events for change_book
    Added event listeners for change_book element to handle mouseover and mouseout events.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ document.addEventListener("mousemove",function(e) {
 
 const change1 = document.getElementById('change')
 const change_social = document.getElementById('change_social')
+const cnange_book = document.getElementById('change_book')
 
 change1.addEventListener("mouseover", function(){ 
 change1.style.backgroundImage = "url('indeximg/hovercat.gif')"
@@ -23,6 +24,15 @@ change_social.style.backgroundSize = "contain"
 
 change_social.addEventListener('mouseout', function(){ 
 change_social.style.backgroundImage = "none"
+});
+
+change_book.addEventListener("mouseover", function(){ 
+change_book.style.backgroundImage = "url('indeximg/book.webp')"
+change_book.style.backgroundSize = "contain"
+});
+
+change_book.addEventListener('mouseout', function(){ 
+change_book.style.backgroundImage = "none"
 });
 
 
